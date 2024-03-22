6b04163aead390b66f9ab1e3633200f4cb529cc5
tanvirhossain10 <tanvirhossen808@gmail.com>
2024-03-23 00:24:52 +0600

    add the model to the scene

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,52 @@
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import Loader from "../components/Loader";
+import { Island } from "../models/Island";
+
 const Home = () => {
+    const adjustIslandFormScreenSize = () => {
+        let screenScale
+        let screenPosition = [0, -6.5, -43]
+        let rotation = [0.1, 4.7, 0]
+        if (window.innerWidth < 768) {
+            screenScale = [0.9, 0.9, 0.9]
+        }
+        else {
+            screenScale = [1, 1, 1]
+        }
+        return [screenScale, screenPosition, rotation]
+
+    }
+    const [islandScale, islandPostion, islandRotation] = adjustIslandFormScreenSize()
+
     return (
         <section className="w-full h-screen relative">
-            <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+            {/*     <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
                 POPUP
 
 
-            </div>
+            </div> */}
+            <Canvas className="w-full h-screen bg-transparent"
+                camera={{
+                    near: 0.1, far: 1000
+                }}
+            >
+                <Suspense fallback={<Loader />}>
+                    {/* <directionalLight /> */}
+                    <ambientLight />
+                    <Island position={islandPostion}
+
+                        scale={islandScale}
+                        rotation={islandRotation}
+                    />
+                </Suspense>
+
+
+
+            </Canvas>
+
+
+
         </section>
     );
 };
