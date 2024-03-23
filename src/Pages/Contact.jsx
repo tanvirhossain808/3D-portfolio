a99835d2241e784459883c225f99a80f045e1a0a
tanvirhossain10 <tanvirhossen808@gmail.com>
2024-03-23 17:15:08 +0600

    initialize form js

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
+    const formRef = useRef(null)
     const [form, setForm] = useState(
         {
             name: "",
@@ -9,14 +11,45 @@ const Contact = () => {
         }
     )
     const [isLoading, setIsLoading] = useState(false)
-    const handleChange = () => {
-
+    const handleChange = (e) => {
+        setForm({
+            ...form, [e.target.name]: e.target.value
+        })
     }
-
-    const handleFocus = () => {
+    const handleFocus = (e) => {
+        e.preventDefault()
+        // setIsLoading(true)
 
     }
     const handleBlur = () => {
+
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setIsLoading(true)
+        emailjs.send(
+            import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+            import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+            {
+                form_name: form.name,
+                to_name: "Tanvir",
+                from_email: form.email,
+                to_email: "tanvirhossen808@gmail.com",
+                message: form.message
+            },
+            import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        ).then(() => {
+            setIsLoading(false)
+            setForm({
+                name: "", email: "", message: ""
+            })
+            //Todo:show success message
+            //todo:hide and alert
+        }).catch((error) => {
+            setIsLoading(false)
+            console.log(error);
+            //todo:show error message
+        })
 
     }
 
@@ -24,7 +57,11 @@ const Contact = () => {
         <section className="relative flex lg:flex-row flex-col max-container">
             <div className="flex-1 min-w-[50%] flex flex-col">
                 <h1 className="head-text">Get in Touch</h1>
-                <form className="w-full flex flex-col gap-7 mt-14">
+                <form className="w-full flex flex-col gap-7 mt-14"
+                    onSubmit={handleSubmit}
+
+
+                >
                     <label className="text-black-500 font-semibold">
                         Name
                         <input type="text" name="name" className="input"
