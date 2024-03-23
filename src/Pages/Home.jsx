0e1875efbd063d36c1d74704859f1cf783cdf23e
tanvirhossain10 <tanvirhossen808@gmail.com>
2024-03-23 15:43:38 +0600

    add details about me with help of reusable component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,6 +6,7 @@ import { Island } from "../models/Island";
 import Sky from "../models/Sky";
 import Bird from "../models/Bird";
 import Plane from "../models/Plane";
+import HomeInfo from "./HomeInfo";
 
 const Home = () => {
     const [isRotating, setIsRotating] = useState(false)
@@ -42,11 +43,11 @@ const Home = () => {
     const [planeScale, planePosition] = adjustPlanecreenSize()
     return (
         <section className="w-full h-screen relative">
-            {/*     <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
-                POPUP
+            <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+                {currentStage && <HomeInfo currentStage={currentStage} />}
 
 
-            </div> */}
+            </div>
             <Canvas className={`w-full h-screen bg-transparent ${isRotating ? "cursor-grabbing" : "cursor-grab"}`}
                 camera={{
                     near: 0.1, far: 1000
---
+++ src/Pages/HomeInfo.jsx
@@ -0,0 +1,46 @@
+import { Link } from "react-router-dom";
+import { arrow } from "../assets/icons";
+
+/* eslint-disable react/prop-types */
+const InfoBox = ({ text, link, btnText }) => (
+    <div className="info-box">
+        <p className="font-medium sm:text-xl text-center">{text}</p>
+        <Link to={link} className="neo-brutalism-white neo-btn">
+            {btnText}
+            <img src={arrow} className="w-4 h-4 object-contain" />
+        </Link>
+    </div>
+)
+
+
+const renderContent = {
+    1: (<h1 className="sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5">Hi, I am <span className="font-semibold
+    ">Tanvir </span>👋
+        <br />
+        A 3D Web Developer from Bangladesh
+
+    </h1>),
+    2: (<InfoBox text={"Working on many projects to increase many skills along the way"} link={"/about"}
+        btnText="Learn more"
+    />),
+    3: (<InfoBox text={"Build multiple projects to success ove the year.Curios about the impact?"} link={"/projects"}
+        btnText="Visit my portfolio"
+    />),
+    4: (<InfoBox text={"Need a project done or looking for a dev? I'm just few keystrokes away"} link={"/contact"}
+        btnText="Let's talk"
+    />)
+}
+
+
+
+const HomeInfo = ({ currentStage }) => {
+    // return (
+    //     <div>
+    //         home info
+    //     </div>
+    // );
+
+    return renderContent[currentStage] || null
+};
+
+export default HomeInfo;
